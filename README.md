0a1c7e385fdf85cfd49e04ce3a8c07cd31943eb5
HilPan - ^v^ <hilpan.storage25@gmail.com>
2025-11-12 15:34:41 +0000

    Iteration 1

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Robot-Project-Code
+
+Notes
+- Gotta fix dem code logic and a bunch of syntax stuff i think
---
+++ VEX Code.cpp
@@ -0,0 +1,286 @@
+#pragma region VEXcode Generated Robot Configuration
+// Make sure all required headers are included.
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <string.h>
+
+
+#include "vex.h"
+
+using namespace vex;
+
+// Brain should be defined by default
+brain Brain;
+
+
+// START IQ MACROS
+#define waitUntil(condition)                                                   \
+  do {                                                                         \
+    wait(5, msec);                                                             \
+  } while (!(condition))
+
+#define repeat(iterations)                                                     \
+  for (int iterator = 0; iterator < iterations; iterator++)
+// END IQ MACROS
+
+
+// Robot configuration code.
+inertial BrainInertial = inertial();
+touchled TouchLED9 = touchled(PORT9);
+motor MotorLeft = motor(PORT6, false);
+motor MotorRight = motor(PORT1, true);
+touchled TouchLED7 = touchled(PORT7);
+motor armMotor = motor(PORT2, false);
+motor clawMotor = motor(PORT8, false);
+distance Distance12 = distance(PORT12);
+
+
+// generating and setting random seed
+void initializeRandomSeed(){
+  wait(100,msec);
+  double xAxis = BrainInertial.acceleration(xaxis) * 1000;
+  double yAxis = BrainInertial.acceleration(yaxis) * 1000;
+  double zAxis = BrainInertial.acceleration(zaxis) * 1000;
+  // Combine these values into a single integer
+  int seed = int(
+    xAxis + yAxis + zAxis
+  );
+  // Set the seed
+  srand(seed); 
+}
+
+
+
+void vexcodeInit() {
+
+  // Initializing random seed.
+  initializeRandomSeed(); 
+}
+
+#pragma endregion VEXcode Generated Robot Configuration
+
+//----------------------------------------------------------------------------
+//                                                                            
+//    Module:       main.cpp                                                  
+//    Author:       {author}                                                  
+//    Created:      {date}                                                    
+//    Description:  IQ project                                                
+//                                                                            
+//----------------------------------------------------------------------------
+
+// Include the IQ Library
+#include "iq_cpp.h"
+
+// Allows for easier use of the VEX Library
+using namespace vex;
+
+void waitTouch() 
+{
+  while(!TouchLED9.pressing())
+  {}
+  while(TouchLED9.pressing())
+  {}
+}
+
+
+bool touchedIn30S()
+{
+  Brain.Timer.reset();
+  while(Brain.Timer.value()<30)
+  {
+    if(TouchLED9.pressing())
+    {
+      return true;
+    }
+  }
+  return false;
+}
+
+void closeClaw()
+{
+  clawMotor.setPosition(0,turns);
+//  bool piece = (distanceSensor.objectDistance(mm)<130);
+//  if (piece)
+  {
+    clawMotor.spin(reverse, 50, percent);
+    while (clawMotor.position(degrees)<-90)
+    {}
+    clawMotor.stop();
+  }
+ 
+  // else
+  // {
+  //   Brain.Screen.print("No piece detected");
+  // }
+}
+
+void openClaw()
+{
+  clawMotor.setPosition(0,turns);
+  clawMotor.spin(forward,50, percent);
+  while (clawMotor.position(degrees)<90)
+  {}
+  clawMotor.stop(); 
+}
+
+void moveArmForColor(int detectedColor)
+{
+  // detectedColor: 1 = Black, 2 = White
+  if (detectedColor == 1)
+  {  // Black
+    Brain.Screen.newLine();
+    Brain.Screen.print("Box: Black");
+    armMotor.spinToPosition(180, degrees, true); // Changed to blocking for simplicity
+    openClaw(); // your teammate’s claw code
+    armMotor.spinToPosition(0, degrees, true);
+  }
+  else if (detectedColor == 2)
+  { // White
+    Brain.Screen.newLine();
+    Brain.Screen.print("Box: White");
+    armMotor.spinToPosition(120, degrees, true); // Changed to blocking
+    openClaw();
+    armMotor.spinToPosition(0, degrees, true);
+  }
+}
+
+int totalCount(int currentCount) {
+    /*
+    This function as provided has a loop that opens and closes the claw.
+    Per the instructions, we will call it without modification to simply
+    get the incremented count. The actual claw actions for pickup/drop
+    are handled in the main loop logic.
+    */
+    return currentCount + 1;
+}
+
+void driveAlongRow () // drive and pick up
+{
+
+  int detectedColor = -1;
+  int pieceCount = 0;
+
+  if (Distance12.objectDistance(mm) <= 1300) 
+  {
+    MotorLeft.stop();
+    MotorRight.stop();
+                
+    // --- Pickup and Sort Sequence ---
+    openClaw(); // Open claw 
+                
+                
+    closeClaw(); // Grip the piece
+
+                
+    // Check color here
+
+                
+    // Call function to sort the piece.
+    moveArmForColor(detectedColor);
+
+    pieceCount = totalCount(pieceCount);
+                
+    // Resume driving
+    MotorLeft.spin(forward);
+    MotorRight.spin(forward);
+  }
+
+  MotorLeft.stop();
+  MotorRight.stop();
+}
+
+void driveAndTurn(int motorPower, int &detectedColor) 
+{
+    MotorLeft.setVelocity(motorPower,percent);
+    MotorRight.setVelocity(motorPower,percent);
+    for (int row = 0; row < 8; row++) {
+        MotorLeft.spin(forward);
+        MotorRight.spin(forward);
+
+        // Reset the encoder at the start of each row to measure distance
+        MotorLeft.setPosition(0, turns);
+
+        while ((MotorLeft.position*200) < 406) // 16 inches to cm? 
+        {
+            driveAlongRow();
+        }
+
+        if (row < 7) {
+           BrainInertial.setRotation(0,degrees);
+           if (row % 2 == 0) { 
+               BrainInertial.setRotation(0, degrees);
+                 MotorLeft.spin(forward);
+                 MotorRight.spin(reverse);
+                
+           } else { 
+               BrainInertial.setRotation(0, degrees);
+               MotorLeft.spin(reverse);
+               MotorRight.spin(forward);
+              
+           }
+           while (abs(BrainInertial.rotation(degrees)) < abs(angle))
+           {}
+           MotorLeft.stop();
+           MotorRight.stop();
+          
+           MotorLeft.setPosition(0, turns);
+
+           MotorLeft.spin(forward);
+           MotorRight.spin(forward);
+           while((MotorLeft.position(turns) * 200) < )
+           {}
+           MotorLeft.stop();
+           MotorRight.stop();
+
+          if (row % 2 == 0) { // Right turn again
+               BrainInertial.setRotation(0, degrees);
+               MotorLeft.spin(forward); MotorRight.spin(reverse);
+               while(BrainInertial.rotation() < 90) {}
+           } else { // Left turn again
+               BrainInertial.setRotation(0, degrees);
+               MotorLeft.spin(reverse); MotorRight.spin(forward);
+               while(BrainInertial.rotation() > -90) {}
+           }
+           MotorLeft.stop(); MotorRight.stop();
+       }
+   }
+} // or *-1 to turn
+
+
+
+int main() {
+
+  vexcodeInit();
+  // Begin project code
+
+  int detectedColor = -1;
+  int pieceCount = 0;
+
+    Brain.Screen.print("Press Touch LED to begin.");
+    waitTouch();
+
+      Brain.Screen.setCursor(2,1);
+  Brain.Screen.print("Touch the LED Sensor if there’s checkers left on the board.");
+  bool askToRemove = touchedIn30S();
+  if(askToRemove)
+  {
+    Brain.Screen.setCursor(4,1);
+    Brain.Screen.print("Please remove the pieces from the board.");
+  }
+  wait(5,seconds);
+  Brain.Screen.setCursor(6,1);
+  Brain.Screen.print("Please remove the board from the table.");
+
+
+  driveAndTurn();
+
+    
+    Brain.Screen.newLine();
+    Brain.Screen.print("Press Touch LED to exit.");
+    
+    waitTouch();
+
+    return 0;
+}
